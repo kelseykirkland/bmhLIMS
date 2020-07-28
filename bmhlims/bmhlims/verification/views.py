1ce2bc579034ff0e6435dc1a8238cf7a48fa119a
kelseykirkland <kelseykirklandwork@gmail.com>
2020-07-28 13:08:49 -0400

    added view for handling file

---
+++ bmhlims/bmhlims/verification/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import UploadFileForm
+
+# Imaginary function to handle an uploaded file.
+# in verify_samplesheet.py
+#from somewhere import handle_uploaded_file
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # the function I make to handle file v
+            #handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
